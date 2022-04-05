4d7834ce3ddf975a317e28552de2655d9c15e258
Walker <52096849+WalkerWanganping@users.noreply.github.com>
2022-04-05 11:36:41 +0800

    Prac_3

--- temperatures.py
+++ temperatures.py
@@ -3,6 +3,15 @@ CP1404/CP5632 - Practical
 Pseudocode for temperature conversion
 """
 
+
+def temp_change_cel(celsius):
+    fahrenheit = celsius * 9.0 / 5 + 32
+    print("Result: {:.2f} F".format(fahrenheit))
+
+def temp_change_fah(fahrenheit):
+    celsius = (fahrenheit - 32) / 9.0 * 5
+    print("Result: {:.2f} C".format(celsius))
+
 MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
@@ -11,16 +20,15 @@ choice = input(">>> ").upper()
 while choice != "Q":
     if choice == "C":
         celsius = float(input("Celsius: "))
-        fahrenheit = celsius * 9.0 / 5 + 32
-        print("Result: {:.2f} F".format(fahrenheit))
+        temp_change_cel(celsius)
     elif choice == "F":
         fahrenheit = float(input("fahrenheit: "))
-        celsius = (fahrenheit - 32) / 9.0 * 5
-        print("Result: {:.2f} C".format(celsius))
-
+        temp_change_fah(fahrenheit)
     else:
         print("Invalid option")
-    print(MENU)
-    choice = input(">>> ").upper()
+        print(MENU)
+        choice = input(">>> ").upper()
 print("Thank you.")
+
+
 
